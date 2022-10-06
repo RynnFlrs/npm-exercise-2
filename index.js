068c380917b8efdd77647bc3e959a40b63f640b2
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-06 12:24:39 +0800

    Removed unnecesarry displays

--- index.js
+++ index.js
@@ -10,20 +10,13 @@ const joinArgs =  arguments.join(' ');
 
 const argArr = [...joinArgs];
 const vowels = ['a', 'e', 'i', 'o', 'u'];
-const vowelsArr = [];
 let vowelsCtr = 0;
 
 for (let i = 0; i < argArr.length; i++) {
     for (let j = 0; j < vowels.length; j++) {
-        argArr[i] === vowels[j] ? (vowelsArr.push(argArr[i]), vowelsCtr++) : '';
+        argArr[i] === vowels[j] ? vowelsCtr++ : '';
     }
-}
+} 
 
-
-const isAre = (n) => n > 1 ? 'are' : 'is';
-const isPlural = (n) => n > 1 ? 's ' : ' ';
-
-console.log(`The number of argument${isPlural(countArgs)}passed ${isAre(countArgs)} ${countArgs} 
-The joined string of all argument${isPlural(countArgs)}is ${joinArgs}
-The number of vowel${isPlural(vowelsCtr)}inside ${joinArgs} ${isAre(vowelsCtr)} ${vowelsCtr} 
-Such vowel${isPlural(vowelsCtr)}${isAre(vowelsCtr)} ${vowelsArr}`);
+console.log(`The number of argument${countArgs > 1 ? 's ' : ' '}passed ${countArgs > 1 ? 'are' : 'is'} ${countArgs},
+The number of vowel${vowelsCtr > 1 ? 's ' : ' '}inside ${joinArgs} ${vowelsCtr > 1 ? 'are' : 'is'} ${vowelsCtr} `);
