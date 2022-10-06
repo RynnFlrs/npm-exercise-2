09ca92b7403885e8145f3b269897bd19ac84e763
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-06 13:49:15 +0800

    added case insensitive feature for vowels

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const process = require('process');
 const [ , , ...arguments] = process.argv;
 
 const countArgs =  arguments.length;
-const joinArgs =  arguments.join(' ');
+const joinArgs =  arguments.join(' ').toLowerCase();
 
 const argArr = [...joinArgs];
 const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -15,4 +15,4 @@ for (let i = 0; i < argArr.length; i++) {
 } 
 
 console.log(`The number of argument${countArgs > 1 ? 's ' : ' '}passed ${countArgs > 1 ? 'are' : 'is'} ${countArgs},
-The number of vowel${vowelsCtr > 1 ? 's ' : ' '}inside ${joinArgs} ${vowelsCtr > 1 ? 'are' : 'is'} ${vowelsCtr} `);
+The number of vowel${vowelsCtr > 1 ? 's ' : ' '}inside ${joinArgs} ${vowelsCtr > 1 ? 'are' : 'is'} ${vowelsCtr}`);
