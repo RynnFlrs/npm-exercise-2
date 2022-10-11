7075ba221c2ae72084e91eaa2b5284df7e86ba56
Ryann Abillon Flores <ryann.flores@ryann.flores-CPHW762L97>
2022-10-11 08:47:33 +0800

    Installed and applied prettier lint

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const process = require('process');
-const [ , , ...arguments] = process.argv;
+const [, , ...arguments] = process.argv;
 
-const countArgs =  arguments.length;
-const joinArgs =  arguments.join(' ').toLowerCase();
+const countArgs = arguments.length;
+const joinArgs = arguments.join(' ').toLowerCase();
 
 const argArr = [...joinArgs];
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 let vowelsCtr = 0;
 
 for (let i = 0; i < argArr.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-        argArr[i] === vowels[j] ? vowelsCtr++ : '';
-    }
-} 
+  for (let j = 0; j < vowels.length; j++) {
+    argArr[i] === vowels[j] ? vowelsCtr++ : '';
+  }
+}
 
-console.log(`The number of argument${countArgs > 1 ? 's ' : ' '}passed ${countArgs > 1 ? 'are' : 'is'} ${countArgs},
-The number of vowel${vowelsCtr > 1 ? 's ' : ' '}inside ${joinArgs} ${vowelsCtr > 1 ? 'are' : 'is'} ${vowelsCtr}`);
+console.log(`The number of argument${countArgs > 1 ? 's ' : ' '}passed ${
+  countArgs > 1 ? 'are' : 'is'
+} ${countArgs},
+The number of vowel${vowelsCtr > 1 ? 's ' : ' '}inside ${joinArgs} ${
+  vowelsCtr > 1 ? 'are' : 'is'
+} ${vowelsCtr}`);
